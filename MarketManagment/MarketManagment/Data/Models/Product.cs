2036b4ef3d094612b69b4e29f07512b14b242646
faskaa <fazilnb@code.edu.az>
2023-09-15 20:07:53 +0400

    Model's Product added

---
+++ MarketManagment/MarketManagment/Data/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using MarketManagment.Data.Common;
+using MarketManagment.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManagment.Data.Models
+{
+    public class Product : BaseModel
+    {
+        private static int id = 0;
+        public Product()
+        {
+            Id = id;
+            id++;
+        }
+       
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public int Quantity { get; set; }
+    }
+}
