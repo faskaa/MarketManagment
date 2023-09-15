b9251812a9af0d6b1c4693b647c7d79c479c31c3
faskaa <fazilnb@code.edu.az>
2023-09-15 20:17:02 +0400

    Model's Sale added

---
+++ MarketManagment/MarketManagment/Data/Models/Sale.cs
@@ -0,0 +1,26 @@
+﻿using MarketManagment.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManagment.Data.Models
+{
+    public class Sale : BaseModel
+    {
+        private static int id = 0;
+       
+        public Sale()
+        {
+            id = Id;
+            Id++;
+        }
+
+        public decimal Price { get; set; }
+        public SalesItems SalesItems { get; set; }
+        public DateTime Date { get; set; }
+
+
+    }
+}
