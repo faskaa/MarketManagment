2212411f8197df60526b88975f8401f08189dc5f
faskaa <fazilnb@code.edu.az>
2023-09-16 21:55:33 +0400

    Abstract services added

---
+++ MarketManagment/MarketManagment/Services/Abstract/IMarketableService.cs
@@ -0,0 +1,29 @@
+﻿using MarketManagment.Data.Enums;
+using MarketManagment.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManagment.Services.Abstract
+{
+    public interface IMarketableService
+    {       
+        public List<Sale> GetSales();
+        public List<Product> GetProducts();
+        public int AddSale(int id, decimal price, SalesItems salesItems, DateTime date);
+        public int ReturnSale(int id);
+        public int ReturnAllSale(Sale sale);
+        public List<Sale> GetSaleByDateRange(DateTime minDate, DateTime maxDate);
+        public List<Sale> GetSaleByDate(DateTime date);
+        public List<Sale> GetSaleByPriceRange(decimal minPrice, decimal maxPrice);
+        public List<Sale> GetSaleById(int id);
+
+        public int AddProduct(string name, decimal price, Category category, int quantity);
+        public int ChangeProduct(int id);
+        public Category GetCategory(Category category);
+        public List<Product> GetProductByPriceRange(Decimal minPrice, decimal maxPrice);
+        public List<Product> GetProductByPriceName(string name);
+    }
+}
